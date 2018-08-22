b22cfe1e6ada2103fdc631b6d68f07e3e9cb3ed6
mesilov <mesm@rarus.ru>
2018-08-22 13:33:41 +0300

    task#55 исправлена ошибка с получением пустой коллекции карт

--- src/Rarus/BonusServer/Cards/Transport/Role/Organization/Transport.php
+++ src/Rarus/BonusServer/Cards/Transport/Role/Organization/Transport.php
@@ -146,19 +146,25 @@ class Transport extends BonusServer\Transport\AbstractTransport
             'phone' => $user->getPhone(),
         ]);
 
-        $requestResult = $this->apiClient->executeApiRequest(
-            sprintf('/organization/card?%s&calculate_count=true&user_id=%s',
-                BonusServer\Transport\Formatters\Pagination::toRequestUri($pagination),
-                $user->getUserId()->getId()
-            ),
-            RequestMethodInterface::METHOD_GET
-        );
-
         $cardCollection = new BonusServer\Cards\DTO\CardCollection();
-        foreach ((array)$requestResult['cards'] as $card) {
-            $cardCollection->attach(BonusServer\Cards\DTO\Fabric::initCardFromServerResponse($card, $this->getDefaultCurrency(), $this->apiClient->getTimezone()));
+        try {
+            $requestResult = $this->apiClient->executeApiRequest(
+                sprintf('/organization/card?%s&calculate_count=true&user_id=%s',
+                    BonusServer\Transport\Formatters\Pagination::toRequestUri($pagination),
+                    $user->getUserId()->getId()
+                ),
+                RequestMethodInterface::METHOD_GET
+            );
+            foreach ((array)$requestResult['cards'] as $card) {
+                $cardCollection->attach(BonusServer\Cards\DTO\Fabric::initCardFromServerResponse($card, $this->getDefaultCurrency(), $this->apiClient->getTimezone()));
+            }
+            $cardCollection->rewind();
+        } catch (BonusServer\Exceptions\ApiClientException $exception) {
+            // если карты не найдены, то сервер возврашает 404 статус выставив 114 код в данном случае мы его подавляем
+            if ($exception->getCode() !== 114) {
+                throw $exception;
+            }
         }
-        $cardCollection->rewind();
 
         $this->log->debug('rarus.bonus.server.cards.transport.organization.getByUser.finish', [
             'itemsCount' => $cardCollection->count(),
