1fc79cad91c24691b68c9ee43d47bd980fc7d123
ivlean <ivlean@rarus.ru>
2025-09-16 12:25:31 +0300

    refactor(documents): add `purchaseId` field to `DocumentDto`, improve code style and parameter formatting

--- src/Documents/DTO/DocumentDto.php
+++ src/Documents/DTO/DocumentDto.php
@@ -13,10 +13,10 @@ use Rarus\LMS\SDK\Utils\MoneyParser;
 final readonly class DocumentDto
 {
     /**
-     * @param  array<CertificatePaymentDto>|null  $certificatePayment
-     * @param  array<HoldBonusDto>|null  $holdBonuses
-     * @param  array<SaleItemDto>  $items
-     * @param  array<PaymentDto>|null  $payment
+     * @param array<CertificatePaymentDto>|null $certificatePayment
+     * @param array<HoldBonusDto>|null $holdBonuses
+     * @param array<SaleItemDto> $items
+     * @param array<PaymentDto>|null $payment
      */
     public function __construct(
         public string $docNumber,
@@ -34,10 +34,12 @@ final readonly class DocumentDto
         public ?array $payment = null,
         public ?string $promo = null,
         public ?string $saleChannelId = null,
-    ) {}
+        public ?string $purchaseId = null,
+    ) {
+    }
 
     /**
-     * @param  array<string, mixed>  $data
+     * @param array<string, mixed> $data
      *
      * @throws ApiClientException
      */
@@ -48,7 +50,7 @@ final readonly class DocumentDto
             registerId: $data['register_id'],
             shopId: $data['shop_id'],
             items: array_map(
-                fn (array $item) => SaleItemDto::fromArray($item, $currency, $dateTimeZone),
+                fn(array $item) => SaleItemDto::fromArray($item, $currency, $dateTimeZone),
                 $data['items']
             ),
             localDate: isset($data['local_date']) ? DateTimeParser::fromTimestamp(
@@ -61,22 +63,23 @@ final readonly class DocumentDto
             ) : null,
             cardId: $data['card_id'] ?? null,
             certificatePayment: isset($data['certificate_payment']) ? array_map(
-                fn (array $c) => CertificatePaymentDto::fromArray($c, $currency),
+                fn(array $c) => CertificatePaymentDto::fromArray($c, $currency),
                 $data['certificate_payment']
             ) : null,
             date: isset($data['date']) ? DateTimeParser::fromTimestamp($data['date'], $dateTimeZone) : null,
             holdBonuses: isset($data['hold_bonuses']) ? array_map(
-                fn (array $b) => HoldBonusDto::fromArray($b, $currency),
+                fn(array $b) => HoldBonusDto::fromArray($b, $currency),
                 $data['hold_bonuses']
             ) : null,
             holdPromoId: $data['hold_promo_id'] ?? null,
             id: $data['id'] ?? null,
             payment: isset($data['payment']) ? array_map(
-                fn (array $p) => PaymentDto::fromArray($p, $currency),
+                fn(array $p) => PaymentDto::fromArray($p, $currency),
                 $data['payment']
             ) : null,
             promo: $data['promo'] ?? null,
             saleChannelId: $data['sale_channel_id'] ?? null,
+            purchaseId: $data['purchase_id'] ?? null,
         );
     }
 
@@ -89,30 +92,31 @@ final readonly class DocumentDto
             'bonus_discount' => $this->bonusDiscount ? MoneyParser::toString($this->bonusDiscount) : 0,
             'card_id' => $this->cardId,
             'certificate_payment' => $this->certificatePayment ? array_map(
-                fn (CertificatePaymentDto $c) => $c->toArray(),
+                fn(CertificatePaymentDto $c) => $c->toArray(),
                 $this->certificatePayment
             ) : null,
             'date' => $this->date ? DateTimeParser::toTimestamp($this->date) : null,
             'doc_number' => $this->docNumber,
             'hold_bonuses' => $this->holdBonuses ? array_map(
-                fn (HoldBonusDto $b) => $b->toArray(),
+                fn(HoldBonusDto $b) => $b->toArray(),
                 $this->holdBonuses
             ) : null,
             'hold_promo_id' => $this->holdPromoId,
             'id' => $this->id,
             'items' => array_map(
-                fn (SaleItemDto $i) => $i->toArray(),
+                fn(SaleItemDto $i) => $i->toArray(),
                 $this->items
             ),
             'local_date' => $this->localDate?->format(DATE_ATOM),
             'payment' => $this->payment ? array_map(
-                fn (PaymentDto $p) => $p->toArray(),
+                fn(PaymentDto $p) => $p->toArray(),
                 $this->payment
             ) : null,
             'promo' => $this->promo,
             'register_id' => $this->registerId,
             'sale_channel_id' => $this->saleChannelId,
             'shop_id' => $this->shopId,
+            'purchase_id' => $this->purchaseId,
         ];
     }
 }
