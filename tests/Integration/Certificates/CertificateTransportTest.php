9fe597edb46fa54eb5a8bbd1f7c97cca4246e7b7
ivlean <ivlean@rarus.ru>
2026-02-25 10:02:31 +0300

    feat(certificates): add certificate creation endpoint + integration test

--- tests/Integration/Certificates/CertificateTransportTest.php
+++ tests/Integration/Certificates/CertificateTransportTest.php
@@ -3,13 +3,18 @@
 namespace Integration\Certificates;
 
 use Exception;
+use Money\Currency;
 use PHPUnit\Framework\TestCase;
 use Psr\SimpleCache\InvalidArgumentException;
 use Rarus\LMS\SDK\Certificates\DTO\CertificateDto;
 use Rarus\LMS\SDK\Client;
+use Rarus\LMS\SDK\Documents\DTO\CertificateDocumentDto;
+use Rarus\LMS\SDK\Documents\DTO\DocumentDto;
+use Rarus\LMS\SDK\Documents\DTO\SaleItemDto;
 use Rarus\LMS\SDK\Exceptions\ApiClientException;
 use Rarus\LMS\SDK\Exceptions\NetworkException;
 use Rarus\LMS\SDK\Exceptions\UnknownException;
+use Rarus\LMS\SDK\Utils\MoneyParser;
 use TestEnvironmentManager;
 
 class CertificateTransportTest extends TestCase
@@ -26,6 +31,38 @@ class CertificateTransportTest extends TestCase
         $code = 'PROMO-103';
         $certificateDto = $this->client->certificates()->getCertificateById($code);
         $this->assertInstanceOf(CertificateDto::class, $certificateDto);
+    }
+
+    public function test_create_certificate(): void
+    {
+        $code = 'TEST_' . random_int(100000000, 999999999);
+        $parentId = '1';
+        $newCertificateId = $this->client->certificates()->createCertificate($code, $parentId);
+        $certificateDto = $this->client->certificates()->getCertificateById($code);
+        $this->assertEquals($newCertificateId, $certificateDto->code);
+
+        date_default_timezone_set('Europe/Moscow');
+        $saleItemDto = new SaleItemDto(
+            productId: 'id-1',
+            productName: 'name-1',
+            price: MoneyParser::parse('100', new Currency('RUB')),
+            quantity: 1,
+            certificates: [
+                new CertificateDocumentDto(
+                    balance: MoneyParser::parse('100', new Currency('RUB')),
+                    code: $code,
+                )
+            ]
+        );
+        $products = [$saleItemDto];
+        $documentDto = new DocumentDto(
+            docNumber: '1234567890',
+            registerId: '1234567890',
+            shopId: '1',
+            items: $products,
+        );
+        $result = $this->client->documents()->purchase($documentDto);
+        $this->assertNotEmpty($result['items']);
     }
 
     /**
